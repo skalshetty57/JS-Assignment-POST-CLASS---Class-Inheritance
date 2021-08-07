a813aac117caf87d1bec69c5ef236be5599e0a68
skalshetty57 <74254221+skalshetty57@users.noreply.github.com>
2021-08-07 12:42:20 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+modeule.exports = class Shape{
+    constructor(c){
+       let color=c===undefined?"blue":c;
+    }
+  
+  function drawShape(){
+    console.log("draw the shape"); 
+  }
+  function calculateArea(){
+   console.log("area is area"); 
+  }
+}
+
 
 // export class using module.exports

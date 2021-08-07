76e3c1fa3b36db53e86982f88a750e0aba1b0784
skalshetty57 <74254221+skalshetty57@users.noreply.github.com>
2021-08-07 12:46:09 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
-
+var msg = require('./shape.js');
 // declare class
-
+module.exports=class Circle extends msg{
+ constructor(...args){
+  super(...args); 
+ }
+  function calculateArea(){
+    console.log("2*pi*r"); 
+  }
+}
 
 // export class using module.exports
